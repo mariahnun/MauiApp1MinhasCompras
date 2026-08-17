1f8d5ae05da06390383a753eee09f9eddcb4d521
Maria <mariahnunnn@gmail.com>
2026-08-17 08:24:39 -0300

    Construção da classe para fazer CRUD no SQLite

--- MauiApp1MinhasCompras/App.xaml.cs
+++ MauiApp1MinhasCompras/App.xaml.cs
@@ -12,6 +12,8 @@ namespace MauiApp1MinhasCompras
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
+
+                MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
 }
--- MauiApp1MinhasCompras/Models/Produto.cs
+++ MauiApp1MinhasCompras/Models/Produto.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using
+using SQLite;
+
 
 namespace MauiApp1MinhasCompras.Models
 {
-    internal class Produto
+    public class Produto
+
     {
+        [PrimaryKey, AutoIncrement]
+
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+        public double Quantidade { get; set; }
+
+        public double Preco { get; set; }
+
+
+
     }
 }
