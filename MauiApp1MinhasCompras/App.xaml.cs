6e2e1ed51ab98337818e8ae6c208cdfe6b561414
Maria <mariahnunnn@gmail.com>
2026-08-23 23:09:54 -0300

    Início da implementação do front

--- MauiApp1MinhasCompras/App.xaml.cs
+++ MauiApp1MinhasCompras/App.xaml.cs
@@ -1,19 +1,38 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MauiApp1MinhasCompras.Helpers;
 
 namespace MauiApp1MinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+
+            {
+                if (_db == null)
+
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+
+
+            }
+
+        }
+
         public App()
         {
             InitializeComponent();
-        }
-
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-
-                MainPage = new NavigationPage(new Views.ListaProduto());
+            MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
 }
