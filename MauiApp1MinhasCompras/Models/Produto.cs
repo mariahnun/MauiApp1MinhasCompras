7a25b35def8775505c561ca4aeffa95130a00d22
Maria <mariahnunnn@gmail.com>
2026-09-08 14:54:32 -0300

    Implementação Ag5

--- MauiApp1MinhasCompras/Models/Produto.cs
+++ MauiApp1MinhasCompras/Models/Produto.cs
@@ -9,10 +9,23 @@ namespace MauiApp1MinhasCompras.Models
     public class Produto
 
     {
+        string _descricao;
+
         [PrimaryKey, AutoIncrement]
 
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao { 
+            get => _descricao;
+            set 
+            {
+                if (value == null) 
+                {
+                    throw new ArgumentException("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+        }
 
         public double Quantidade { get; set; }
 
